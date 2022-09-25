2e4f8fa5f286add6483e90fa9096da104ddbcf1c
Gregory <gregoryKot@gmail.com>
2022-09-25 15:46:09 +0300

    creating service

---
+++ src/messages/messages.service.ts
@@ -0,0 +1,19 @@
+import { MessagesRepository } from './message.repository';
+
+export class MessagesService {
+  messagesRepo: MessagesRepository;
+  constructor() {
+    this.messagesRepo = new MessagesRepository();
+  }
+  async findOne(id: string) {
+    return await this.messagesRepo.findOne(id);
+  }
+
+  async findAll() {
+    return await this.messagesRepo.findAll();
+  }
+
+  async createMessage(content: string) {
+    return await this.messagesRepo.create(content);
+  }
+}
